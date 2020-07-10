70c38931ae8f07608339a7c0b8ec863e53dbaf31
Luke P <luke@lpdev.io>
2020-07-11 09:31:45 +1000

    Preparing API url

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,25 @@ function Home() {
   
   const [wikiData, setWikiData] = useState(null);
 
-  useEffect(() => {
-    fetchWikiData().then(data => {
-      setWikiData(data);
-    });
-  }, []);
+  // useEffect(() => {
+  //   fetchWikiData().then(data => {
+  //     setWikiData(data);
+  //   });
+  // }, []);
+
+  fetchWikiData({
+    format: 'json',
+    action: 'parse',
+    page: 'COVID-19_pandemic_in_Australia'
+  });
 
   return (
     <>
       <h1>Fetching Wikipedia Content</h1>
 
       <p>I will be getting and parsing data from wikipedia using the public API.</p>
+
+      <hr/>
 
       <p>{wikiData || 'Data here...'}</p>
 
